b6d9efd3935811eb013691037a20ca13aed10675
nodnarb1129 <34693907+nodnarb1129@users.noreply.github.com>
2018-11-12 13:51:36 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!-- This is my Home page -->
+<html> 
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>YOLO|Home</title>
+        <link rel="icon" type="image/x-generic" href="http://www.brandonsandall.com/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="../../css/fhe.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <h1>Home</h1>
+                    <?php include($_SERVER['DOCUMENT_ROOT']."/camp/common/mainbar.php"); ?>
+            </nav>
+        </header>
+        <br>
+        <br>
+        <h2>
+        Get started with the list above.
+        </h2>
+        <div style="text-align:center; margin-bottom:-25px">
+        <img src="../images/home.png" alt="Home">
+        </div><br>
+        <footer style="text-align: center">
+            <p>Check out my YouTube channel for these and others
+            <a href="http://www.youtube.com/user/YOLO51212?ob=0" style="color:black;text-decoration:underline">
+            here</a>.</p>
+            <br>
+            <a href="http://www.brandonsandall.com/assignments/siteplan.php" style="color:black">Site Plan</a> |
+            <a href="http://www.brandonsandall.com/assignments/styleguide.php" style="color:black">Style Guide</a> |
+            <a href="http://www.brandonsandall.com/colophon" style="color:black">Colophon</a> |
+            <a href="http://www.brandonsandall.com/class/contact" style="color:black">Contact</a> |
+            <?php echo "Last updated: " . date("M d, Y, h:i:s a", getlastmod()) ?>
+        </footer>
+    </body>
+</html>
